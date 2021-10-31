1ed7165676439b46cb06fa2467d79931c9722ae1
Marc from GitKraken <marcmauri92@gmail.com>
2021-10-31 17:37:23 +0100

    Add data clases
    Unsplash model classes: UnsplashPhoto, UnsplashUser, UnsplashCamera

---
+++ app/src/main/java/es/marcmauri/photobook/features/photogrid/model/UnsplashCamera.kt
@@ -0,0 +1,24 @@
+package es.marcmauri.photobook.features.photogrid.model
+
+import android.os.Parcel
+import android.os.Parcelable
+
+data class UnsplashCamera(
+    val id: String
+) : Parcelable {
+
+    constructor(parcel: Parcel) : this(
+        parcel.readString()!!
+    )
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(id)
+    }
+
+    override fun describeContents() = 0
+
+    companion object CREATOR : Parcelable.Creator<UnsplashCamera> {
+        override fun createFromParcel(parcel: Parcel) = UnsplashCamera(parcel)
+        override fun newArray(size: Int) = arrayOfNulls<UnsplashCamera?>(size)
+    }
+}
